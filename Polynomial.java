92748045d1affc18fb1a8e49d994076bf3f4f346
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-09-16 15:41:34 -0400

    removed redundant length method

--- Polynomial.java
+++ Polynomial.java
@@ -12,21 +12,15 @@ public class Polynomial {
 		}
 	}
 
-	// helper function to find length of array
-	public int length(){
-		//since Java has built-in function to find length for array
-		return polynomialCo.length; 
-	}
-
 	public Polynomial add(Polynomial p){
 		// add polynomialCo and p
-		int maxLength = Math.max(this.length(), p.length());
+		int maxLength = Math.max(polynomialCo.length, p.polynomialCo.length);
 		double[] result = new double[maxLength];
 		for (int i = 0; i < maxLength; i++){
 			if (i >= polynomialCo.length){
 				result[i] = 0 + p.polynomialCo[i];
 			}
-			else if (i >= p.length()) {
+			else if (i >= p.polynomialCo.length) {
 				result[i] = polynomialCo[i] + 0;
 			}
 			else{
