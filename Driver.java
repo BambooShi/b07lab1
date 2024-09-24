bfb2b98f6369cd7330b90809e6861e39321e7f86
BambooShi <110565810+BambooShi@users.noreply.github.com>
2024-09-24 01:21:36 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,43 @@
+import java.util.HashMap;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Driver {
 public static void main(String [] args) {
 Polynomial p = new Polynomial();
 System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
+double [] c1 = {6,5};
+int [] pc1 = {0,3};
+Polynomial p1 = new Polynomial(c1, pc1);
+double [] c2 = {-2,-9};
+int [] pc2 = {1, 4};
+Polynomial p2 = new Polynomial(c2, pc2);
 Polynomial s = p1.add(p2);
 System.out.println("s(0.1) = " + s.evaluate(0.1));
 if(s.hasRoot(1))
 System.out.println("1 is a root of s");
 else
 System.out.println("1 is not a root of s");
+
+Polynomial m = p1.multiply(p2);
+
+System.out.println("m is: " + m.polynomial.keySet() + " and " + m.polynomial.values());
+
+File file = new File("C:\\Users\\shiSn\\b07lab1\\polyTest.txt");
+
+try {
+    Polynomial p3 = new Polynomial(file);  // Initialize polynomial from file
+    System.out.println("p3 is: " + p3.polynomial.keySet() + " and " + p3.polynomial.values());
+} catch (FileNotFoundException e) {
+    System.out.println("File not found: " + e.getMessage());  // Handle file not found error
+}
+
+try {
+    s.saveToFile("C:\\Users\\shiSn\\b07lab1\\polyCopy.txt");
+} catch (IOException e) {
+    System.out.println("File not found: " + e.getMessage());  // Handle file not found error
+}
+
 }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,40 +1,143 @@
+import java.util.HashMap;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Polynomial {
 	double[] polynomialCo;
+	int[] polynomialDegree;
+	HashMap<Integer, Double> polynomial = new HashMap<Integer, Double>();
 
 	public Polynomial(){
 		polynomialCo = new double[]{0};
+		polynomialDegree = new int[]{0};
+		polynomial.put(0,0.0);
 	}
 
-	public Polynomial(double[] poly){
-		this.polynomialCo = new double[poly.length];
-		for (int i = 0; i < poly.length; i++){
-			polynomialCo[i] = poly[i];
+	public Polynomial(double[] polyCo, int[] polyD){
+		this.polynomialCo = polyCo;
+		this.polynomialDegree = polyD;
+		for (int i = 0; i < polyCo.length; i++){
+			this.polynomial.put(polyD[i], polyCo[i]);
 		}
+		
+	}
+
+	public Polynomial(File fileObj) throws FileNotFoundException {
+		Scanner scanner = new Scanner(fileObj);
+		String polyString = scanner.nextLine();
+		scanner.close();
+
+		polyString = polyString.replace("-", "+-");
+
+		String[] terms = polyString.split("\\+");
+
+		HashMap<Integer, Double> result = new HashMap<>();
+
+		for (String term : terms) {
+			if(!term.isEmpty()) {
+				if (!term.contains("x")) {
+					result.put(0, Double.parseDouble(term));
+				} else {
+					String[] parts = term.split("x");
+					double coefficient;
+					int degree;
+
+					if(parts[0].isEmpty() || parts[0].equals("+")) {
+						coefficient = 1.0;
+					} else if (parts[0].equals("-")) {
+						coefficient = -1.0;
+					} else {
+						coefficient = Double.parseDouble(parts[0]);
+					}
+
+					if (parts.length == 1){
+						degree = 1;
+					} else {
+						degree = Integer.parseInt(parts[1]);
+					}
+
+					result.put(degree, result.getOrDefault(degree, 0.0) + coefficient);
+				}
+			}
+		}
+		
+		polynomialCo = new double[result.size()];
+		polynomialDegree = new int[result.size()];
+
+		int index = 0;
+		for (int deg : result.keySet()){
+			polynomialDegree[index] = deg;
+			polynomialCo[index] = result.get(deg);
+			index++;
+		}
+
+		for (int i = 0; i < polynomialCo.length; i++){
+			this.polynomial.put(polynomialDegree[i], polynomialCo[i]);
+		}
+	}
+
+	public Boolean containsD (int[] polyD, int item){
+		for (int i: polyD){
+			if (item == i){	
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public Boolean containsD (Polynomial polyD, int item){
+		for (int i: polyD.polynomial.keySet()){
+			if (item == i) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public Polynomial add(Polynomial p){
-		// add polynomialCo and p
-		int maxLength = Math.max(polynomialCo.length, p.polynomialCo.length);
-		double[] result = new double[maxLength];
-		for (int i = 0; i < maxLength; i++){
-			if (i >= polynomialCo.length){
-				result[i] = 0 + p.polynomialCo[i];
-			}
-			else if (i >= p.polynomialCo.length) {
-				result[i] = polynomialCo[i] + 0;
-			}
-			else{
-				result[i] = polynomialCo[i] + p.polynomialCo[i];
-			}
+		HashMap<Integer, Double> result = new HashMap<Integer, Double>();
+
+		for (int i = 0; i < this.polynomialDegree.length; i++) {
+			int  degree = this.polynomialDegree[i];
+			double coefficient = this.polynomialCo[i];
+			result.put(degree, coefficient);
 		}
-		return new Polynomial(result);
+
+
+		for (int i = 0; i < p.polynomialDegree.length; i++) {
+			int  degree = p.polynomialDegree[i];
+			double coefficient = p.polynomialCo[i];
+			if (containsD(p.polynomialDegree, degree) && containsD(this.polynomialDegree, degree)){
+				result.put(degree, coefficient + this.polynomial.get(i));
+
+			} else {
+				result.put(degree, coefficient);
+			}
+			
+		}
+		
+		double[] newPolyC = new double[result.size()];
+		int[]  newPolyD = new int[result.size()];
+
+		int index = 0;
+
+		for (int deg : result.keySet()){
+			newPolyD[index] = deg;
+			newPolyC[index] = result.get(deg);
+			index++;
+		}
+
+		return new Polynomial(newPolyC, newPolyD);
 	}
 	
 	public double evaluate(double x){
 		// should sub value 'x' into polynomial and return final value
 		double value = 0;
-		for (int i = 0; i < polynomialCo.length; i++){
-			value += Math.pow(x, i) * polynomialCo[i];
+		for (int i = 0; i < polynomialDegree.length; i++){
+			value += Math.pow(x, polynomialDegree[i]) * polynomialCo[i];
 		}
 		return value;
 	}
@@ -43,4 +146,49 @@ public class Polynomial {
 		// return true if evaluate(root) = 0
 		return evaluate(root) == 0;
 	}
+
+	public Polynomial multiply(Polynomial p){
+		Polynomial newP = new Polynomial();
+
+		for (int i = 0; i < this.polynomial.size(); i++){
+			for (int j = 0; j < p.polynomial.size(); j++){
+				double newCo = this.polynomialCo[i] * p.polynomialCo[j];
+				int newD = this.polynomialDegree[i] + p.polynomialDegree[j];
+				
+				if (containsD(newP, newD)){
+					newP.polynomial.put(newD, newP.polynomial.get(newD) + newCo);
+				} else {	
+					newP.polynomial.put(newD, newCo);
+				}
+			}	
+		}
+		
+		newP.polynomial.remove(0);
+
+		return newP;
+	}
+
+	public void saveToFile(String fileName) throws IOException {
+		StringBuilder polyStr = new StringBuilder();
+		for(int i = 0; i < polynomialCo.length; i++){
+			double co = polynomialCo[i];
+			int degree = polynomialDegree[i];
+
+			if (i != 0 && co >= 0) {
+				polyStr.append("+");
+			}
+			if (degree == 0) {
+				polyStr.append(co);
+			} else {
+				polyStr.append(co).append("x");
+				if (degree != 1) {
+					polyStr.append(degree);
+				}
+			}
+		}
+		FileWriter writer = new FileWriter(fileName);
+		writer.write(polyStr.toString());
+		writer.close();
+		
+	}	
 }
